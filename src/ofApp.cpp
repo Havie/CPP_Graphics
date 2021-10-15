689077cdf7e27f0e5cd0497f4ff89a0172fbadfa
Logan Steffen <55418381+Eless0@users.noreply.github.com>
2021-10-15 16:19:49 -0500

    Finished project 2

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -11,9 +11,21 @@ void ofApp::setup(){
 
 }
 
+void ofApp::updateCameraRotation(float dx, float dy) {
+	cameraHead += dx;
+	cameraPitch += dy;
+}
+
 //--------------------------------------------------------------
-void ofApp::update(){
-	
+void ofApp::update()
+{
+	using namespace glm;
+	//Do the opposite/Inverse of the view matrix to calculate world space velo
+	vec3 velocityWorldSpace = mat3((rotate(-cameraHead, vec3(0, 1, 0)) * rotate(-cameraPitch, vec3(1, 0, 0)))) * velocity;
+
+	float dt = static_cast<float>(ofGetLastFrameTime());
+	position += velocityWorldSpace * dt;
+
 }
 
 //--------------------------------------------------------------
@@ -24,9 +36,12 @@ void ofApp::draw(){
 	float height = static_cast<float>(ofGetViewportHeight());
 	float aspect = width / height;
 
+	//changes to model position
 	mat4 model = translate(vec3(-3, -3, -7)) * rotate(radians(-45.0f), vec3(-0.25, 1, 0.25));
-	mat4 view;
-	mat4 proj = perspective(radians(100.0f), aspect, 0.01f, 10.0f);
+	//changes to camera position, head rotates around y axis, pitch rotates around x axis
+	mat4 view = (rotate(cameraPitch, vec3(1, 0, 0)) * rotate(cameraHead, vec3(0,1,0)) * translate(-position));
+	//sets perspective type and near/far camera clip planes
+	mat4 proj = perspective(radians(FOV), aspect, cameraClipNear, cameraClipFar);
 
 	shader.begin();
 
@@ -43,17 +58,62 @@ void ofApp::draw(){
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+	//z-axis	
+	if (key== 'w')
+	{
+		velocity.z = -1;
+	}
+	else if (key== 's')
+	{
+		velocity.z = 1;
+	}
+	//x-axis
+	else if (key== 'a')
+	{
+		velocity.x = -1;
+	}
+	else if (key== 'd')
+	{
+		velocity.x = 1;
+	}
+	//Y-axis
+	else if (key== 'q')
+	{
+		velocity.y = -1;
+	}
+	else if (key== 'e')
+	{
+		velocity.y = 1;
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
 
+	//z-axis	
+	if (key== 'w' || key== 's')
+	{
+		velocity.z = 0;
+	}
+	//x-axis	
+	if (key== 'a' || key== 'd')
+	{
+		velocity.x = 0;
+	}
+	//y-axis	
+	if (key== 'q' || key== 'e')
+	{
+		velocity.y = 0;
+	}
 }
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
-
+	if (prevX != 0 && prevY != 0) {
+		updateCameraRotation(mouseSensitivity * (x - prevX), mouseSensitivity * (y - prevY));
+	}
+	prevX = x;
+	prevY = y;
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -26,4 +26,30 @@ private:
 	ofMesh loganMesh;
 	ofMesh steveMesh;
 	glm::mat4 worldMatrix;
+
+	//x and y coordinates of the mouse from previous frame
+	int prevX;
+	int prevY;
+
+	//Movement WASD / QE - cam space
+	glm::vec3 velocity;
+	glm::vec3 position;
+	//sensitivity when moving camera with mouse
+	float mouseSensitivity = 0.05f;
+
+	//current head direcion of camera
+	float cameraHead;
+	
+	//current pitch direction of camera
+	float cameraPitch;
+
+	//near and far variables for camera clip planes
+	float cameraClipNear = 0.01f;
+	float cameraClipFar = 10.0f;
+
+	//variable for camera field of view
+	float FOV = 100.0f;
+
+	//update roations of the camera based on given x y changes
+	void updateCameraRotation(float dx, float dy);
 };
